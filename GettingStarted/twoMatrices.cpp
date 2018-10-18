902f8f4061f807eed93a9758ddcfd750e730b5df
Jose Luis Hernandez Camacho <jl.hernandezcamacho@ugto.mx>
2018-10-18 18:16:09 -0500

    The file twoMatrices.cpp have two matrices declarated of dimension of 3x3

---
+++ GettingStarted/twoMatrices.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //Declare the two matrices
+              
+    //Matriz 3x3 in the two cases
+             //rows -- columns
+    int matriz  [3]     [3]; 
+    int matriz2 [5]     [5];
+    
+    return 0;
+    
+}
